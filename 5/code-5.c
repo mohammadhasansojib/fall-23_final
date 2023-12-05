57d1e77b3d00f3d7a8ead3287a4010458910f447
sajib hasan <sajibhasan800080@gmail.com>
2023-12-05 23:51:42 +0600

    solved problem 5

--- 5/code-5.c
+++ 5/code-5.c
@@ -1,1 +1,48 @@
+/*
+
+Write a program to find the sum of the given series using the function.
+1!/1 + 2!/2 + 3!/3 + 4!/4 + 5!/5 +...+ n!/n
+
+*/
+#include <stdio.h>
+
+
+int fact(int n){
+    int fact = 1;
+
+    for(int i = n; i > 0; i--){
+        fact *= i;
+    }
+
+    return fact;
+}
+
+
+int series_sum(int n){
+    int s_fact = 0;
+
+    printf("\n");
+    for(int i = 1; i <= n; i++){
+        printf(" + %d!/%d", i,i);
+        s_fact += fact(i) / i;
+    }
+
+    printf(" = %d\n", s_fact);
+}
+
+
+
+int main(){
+
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    series_sum(n);
+
+    return 0;
+}
+
+
+
 
