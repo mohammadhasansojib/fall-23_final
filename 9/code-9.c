abc5dfe7da6e37f4ab1ac23ae17fb6f5c14fa071
sajib hasan <sajibhasan800080@gmail.com>
2023-12-08 01:16:39 +0600

    solved problem 9

--- 9/code-9.c
+++ 9/code-9.c
@@ -1,1 +1,24 @@
+/* A prime integer is entered through the keyboard.
+ Write a code to find out the prime factors of the number. */
+ #include <stdio.h>
 
+
+ int prime(int x){
+    for(int i = 2; i <= x; i++){
+        if(x%i == 0){
+            printf("%d ", i);
+            x = x/i;
+            i--;
+        }
+    }
+ }
+
+
+ int main(){
+    int num;
+    scanf("%d", &num);
+
+    prime(num);
+
+    return 0;
+ }
