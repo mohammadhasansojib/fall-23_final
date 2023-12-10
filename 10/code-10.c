73e70617c71cfd3e4d1ed378849e634fddb4bb52
sajib hasan <sajibhasan800080@gmail.com>
2023-12-10 11:42:50 +0600

    solved problem 10

--- 10/code-10.c
+++ 10/code-10.c
@@ -1,8 +1,27 @@
 // Write a program in C to print a string in reverse using a pointer.
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    char str1[100], str2[100];
+    char *p1, *p2;
+    printf("Enter a string: \n");
+    gets(str1);
 
+    p1 = str1 + strlen(str1)-1;
+    p2 = str2;
+
+
+    while(p1 >= str1){
+        *p2 = *p1;
+        p2++;
+        p1--;
+    }
+
+    *p2 = '\0';
+
+    printf("original string; %s\n", str1);
+    printf("Reverse string: %s", str2);
 
 
     return 0;
