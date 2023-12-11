f7d047a873ddd1f9447d519e677a57f864f35bde
sajib hasan <sajibhasan800080@gmail.com>
2023-12-11 10:25:28 +0600

    solved problem 11

--- 11/code-11.c
+++ 11/code-11.c
@@ -1,6 +1,31 @@
+/*Write a C program to find the maximum number of characters of a sentence and print the
+numbers in the terminal by using string.*/
 #include <stdio.h>
 
 int main(){
+
+    int counter,max = 0;
+    char str[100], c, maxchar;
+    printf("Enter an string: \n");
+    gets(str);
+
+    for(c = 'a'; c <= 'z'; c++){
+        counter = 0;
+        for(int i = 0; str[i]!=NULL; i++){
+            if(c==str[i]){
+                counter++;
+
+                if(counter > max){
+                    max = counter;
+                    maxchar = c;
+                }
+
+            }
+        }
+
+    }
+
+    printf("\n%c has maximun time(%d times)\n", maxchar,max);
 
 
     return 0;
