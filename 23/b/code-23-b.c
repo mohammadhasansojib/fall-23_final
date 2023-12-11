ddb8ba6cafe11d750600531c5c76add294652242
sajib hasan <sajibhasan800080@gmail.com>
2023-12-11 17:16:24 +0600

    solved problem 23-b

--- 23/b/code-23-b.c
+++ 23/b/code-23-b.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+int gcd(int a, int b);
+
 int main(){
 
+    int a,b,result;
+    printf("Enter the two numbers to find their GCD: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    result = gcd(a,b);
+    printf("The GCD of %d and %d is %d.\n", a,b,result);
 
     return 0;
+}
+
+int gcd(int a,int b){
+
+    while(a != b){
+        if(a > b){
+            return gcd(a-b, b);
+        }else{
+            return gcd(a, b-a);
+        }
+    }
+
+    return a;
+
 }
