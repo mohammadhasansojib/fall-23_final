fe8e2194bc60e710f09fb202f2c0dcc6379286fd
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 01:33:19 +0600

    added code in code-19.c of '19' folder

--- 19/code-19.c
+++ 19/code-19.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+
+int reverse(int *arr, int n){
+
+int *ptr = arr + n-1;
+
+printf("\nin reverse: ");
+for(int i = 0; i < n; i++){
+    printf("%d ", *ptr);
+    ptr--;
+}
+
+}
+
 int main(){
+
+    int n;
+    printf("enter the array length: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("enter array elements: ");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int *ptr = arr;
+
+    reverse(ptr,n);
 
 
     return 0;
