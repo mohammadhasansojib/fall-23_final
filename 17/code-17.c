a03b2fc475171eb919aadedd9046654f97364332
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 01:03:26 +0600

    added code in code-17.c of '17' folder

--- 17/code-17.c
+++ 17/code-17.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <ctype.h>
+
+int isvowel(char ch){
+    ch = toupper(ch);
+    return (ch == 'A' || ch =='E' || ch =='I' || ch =='O' || ch =='U');
+}
 
 int main(){
+
+    char str[1000];
+    gets(str);
+
+    int number = 0, vowel = 0, consonant = 0, digit = 0, whitespace = 0, word = 0;
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isalpha(str[i]) && isvowel(str[i])){
+            vowel++;
+        }else if(isalpha(str[i])){
+            consonant++;
+        }else if(isdigit(str[i])){
+            digit++;
+        }else if(isspace(str[i])){
+            whitespace++;
+        }
+    }
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isalpha(str[i])){
+            while(isalpha(str[i])){
+                i++;
+            }
+            word++;
+        }
+    }
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isdigit(str[i])){
+            while(isdigit(str[i])){
+                i++;
+            }
+            number++;
+        }
+    }
+
+    printf("\nNumber of number: %d", number);
+    printf("\nNumber of vowels: %d", vowel);
+    printf("\nNumber of consonants: %d", consonant);
+    printf("\nNumber of digits: %d", digit);
+    printf("\nNumber of whitespace characters: %d", whitespace);
+    printf("\nNumber of words: %d", word);
 
 
     return 0;
