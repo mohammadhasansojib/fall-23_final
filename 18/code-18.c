a3e9034b7c2d557fe4c8da28667c7222a6237f29
sajib hasan <sajibhasan800080@gmail.com>
2023-12-14 11:47:44 +0600

    added code in code-18 of '18' folder

--- 18/code-18.c
+++ 18/code-18.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+void swipping(int *ptr1,int *ptr2,int *ptr3){
+
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = *ptr3;
+    *ptr3 = temp;
+}
 
 int main(){
 
+    int a,b,c;
+    printf("enter 3 integer: ");
+    scanf("%d%d%d", &a,&b,&c);
 
+    int *ptr1, *ptr2, *ptr3;
+    ptr1 = &a;
+    ptr2 = &b;
+    ptr3 = &c;
+
+    printf("before swapping- a = %d, b = %d, c = %d\n", *ptr1,*ptr2,*ptr3);
+    swipping(ptr1,ptr2,ptr3);
+    printf("after swapping- a = %d, b = %d, c = %d\n", *ptr1,*ptr2,*ptr3);
 
     return 0;
 }
