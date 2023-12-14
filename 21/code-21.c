b7df8204836471107f313f58ce4f715ccb28c246
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 02:39:35 +0600

    added code in code-21.c of '21' folder

--- 21/code-21.c
+++ 21/code-21.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main(){
+int toggle(char str[]){
 
+    for(int i = 0; str[i] != '\0'; i++){
+        if(islower(str[i])){
+            str[i] = toupper(str[i]);
+        }else if(isupper(str[i])){
+            str[i] = tolower(str[i]);
+        }
+    }
+}
+
+int main(){
+    char str[1000];
+    gets(str);
+
+    toggle(str);
+
+    printf("\nafter toggling: %s", str);
 
     return 0;
 }
