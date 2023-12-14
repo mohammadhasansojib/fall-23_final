d9cd96991cdf1dd0e3ca6cbcb50daf0e530b8b23
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 03:05:03 +0600

    added code in code-22.c of '22' folder

--- 22/code-22.c
+++ 22/code-22.c
@@ -2,6 +2,32 @@
 
 int main(){
 
+    int size,position;
+    printf("enter the size : ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("enter the array elements: ");
+    for(int i = 0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("enter the position for delete: ");
+    scanf("%d", &position);
+
+    if(position < 0 || position >= size){
+        printf("invalid position.");
+    }else{
+        for(int i = position; i < size-1; i++){
+            arr[i] = arr[i+1];
+        }
+        size--;
+    }
+
+    printf("\nafter deletion: ");
+    for(int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
