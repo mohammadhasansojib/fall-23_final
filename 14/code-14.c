6fe37966406e8294b5246c006ebdea4cda3ea2d2
sajib hasan <sajibhasan800080@gmail.com>
2023-12-14 15:13:34 +0600

    added code in code-14.c of '14' folder

--- 14/code-14.c
+++ 14/code-14.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+int fibo(int n){
+
+if(n <= 1){
+    return n;
+}else{
+    return fibo(n-1) + fibo(n-2);
+}
+
+}
+
+
 int main(){
 
+
+    int n;
+    printf("enter a number: ");
+    scanf("%d", &n);
+
+    if(n<0){
+        printf("enter number greater than 0");
+    }else{
+        printf("fibonacci series: ");
+        for(int i = 0; i < n; i++){
+            printf("%d ", fibo(i));
+        }
+    }
 
     return 0;
 }
