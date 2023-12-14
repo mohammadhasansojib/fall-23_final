fd8526015cc0f74dbcc0db8ab239a170ac863717
sajib hasan <sajibhasan800080@gmail.com>
2023-12-14 15:21:17 +0600

    added code in code-13.c of '13' folder

--- 13/code-13.c
+++ 13/code-13.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+
+void decimal_to_binary(int decimal){
+
+    if(decimal == 1){
+        printf("1");
+    }else{
+        decimal_to_binary(decimal/2);
+
+        printf("%d", decimal%2);
+    }
+}
+
+
 int main(){
+
+    int decimal;
+    printf("enter a decimal number: ");
+    scanf("%d", &decimal);
+
+    if(decimal < 0){
+        printf("enter decimal number greater than 0");
+    }else{
+        decimal_to_binary(decimal);
+    }
 
 
     return 0;
