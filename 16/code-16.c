28133f64344096fec079084267b9bc255b13e3a6
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 23:10:39 +0600

    random run or something else

--- 16/code-16.c
+++ 16/code-16.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
-int main(){
+
+
+
+
+
+
+int main() {
+
+    int row1,col1;
+    printf("enter the first maxtrix row and column: ");
+    scanf("%d%d", &row1,&col1);
+
+    int matrix1[row1][col1];
+
+    printf("enter elements of first matrix: ");
+    for(int i = 0; i < row1; i++){
+        for(int j = 0; j < col1; j++){
+            scanf("%d", matrix1[i][j]);
+        }
+    }
+
+
+
+    int row2,col2;
+    printf("enter the second maxtrix row and column: ");
+    scanf("%d%d", &row2,&col2);
+
+    int matrix2[row2][col2];
+
+    if(row1 == col2 && row2 == col1){
+        printf("enter elements of second matrix: ");
+        for(int i = 0; i < row2; i++){
+            for(int j = 0; j < col2; j++){
+                scanf("%d", matrix2[i][j]);
+            }
+        }
+
+        multiply();
+    }else{
+        printf("first matrix row and second matrix column must equal and vice versa.");
+    }
 
 
 
