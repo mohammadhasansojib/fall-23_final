705a9ed793047fcb860c1debda8bcf3d03b3e179
sajib hasan <sajibhasan800080@gmail.com>
2023-12-15 23:05:13 +0600

    random run or something else

--- 6/code-6.c
+++ 6/code-6.c
@@ -1,14 +1,13 @@
 // Write a C program to determine all Pythagorean triplets within a given range between 1 to 1000 using function.
-
 #include <stdio.h>
 
-int pyt_tri(){
+int pyt_tri(int start, int end){
 
     printf("Pythagorean triplets : \n\n");
 
-    for(int a = 1; a <= 1000; a++){
-        for(int b = 1; b <= 1000; b++){
-            for(int c = 1; c <= 1000; c++){
+    for(int a = start; a <= end; a++){
+        for(int b = start; b <= end; b++){
+            for(int c = start; c <= end; c++){
                 if((pow(a,2) + pow(b,2)) == pow(c,2)){
                     printf("%d, %d, %d\n", a,b,c);
                 }
@@ -19,7 +18,15 @@ int pyt_tri(){
 
 int main(){
 
-    pyt_tri();
+    int start,end;
+    printf("enter the start and end range : ");
+    scanf("%d%d", &start,&end);
+
+    if(start >= 1 && start <= 1000 && end >= 1 && end <= 1000){
+        pyt_tri(start, end);
+    }else{
+        printf("enter range between 1 to 1000");
+    }
 
     return 0;
 }
